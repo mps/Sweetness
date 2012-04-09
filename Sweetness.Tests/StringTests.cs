c3307a09ebf7a35e0394ee68a7d657599f272caf
Matthew Strickland <mpstrick@gmail.com>
2012-04-09 11:48:06 -0400

    initial load plus one test and implmentation.

---
+++ Sweetness.Tests/StringTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Sweetness.Tests
+{
+    [TestFixture]
+    public class StringTests
+    {
+        [Test]
+        public void Test_ToDateTime()
+        {
+            "2005-02-27 23:50".ToDateTime().ShouldBe(new DateTime(2005, 2, 27, 23, 50, 0, DateTimeKind.Utc));
+            "2005-02-27 23:50".ToDateTime(true).ShouldBe(new DateTime(2005, 2, 27, 23, 50, 0, DateTimeKind.Local));
+            "2005-02-27T23:50:19".ToDateTime().ShouldBe(new DateTime(2005, 2, 27, 23, 50, 19, DateTimeKind.Utc));
+            "2005-02-27T23:50:19".ToDateTime(true).ShouldBe(new DateTime(2005, 2, 27, 23, 50, 19, DateTimeKind.Local));
+            "".ToDateTime().ShouldBe(null);
+        }
+    }
+}
---
+++ Sweetness/String.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sweetness
+{
+    public static class String
+    {
+        public static DateTime? ToDateTime(this string source, bool local = false)
+        {
+            DateTime date;
+
+            var parsed = DateTime.TryParse(source, out date);
+            if (!parsed)
+                return null;
+
+            DateTime.SpecifyKind(date, local ? DateTimeKind.Local : DateTimeKind.Utc);
+
+            return date;
+        }
+    }
+}
