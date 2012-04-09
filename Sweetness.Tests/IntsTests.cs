c0cd9300279ce7302a9a6f5cf036b915cb19e378
Matthew Strickland <mpstrick@gmail.com>
2012-04-09 15:35:51 -0400

    ints

---
+++ Sweetness.Tests/IntsTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Sweetness.Tests
+{
+    [TestFixture]
+    public class IntsTests
+    {
+        [Test]
+        public void Test_To()
+        {
+            3.To(3).Count().ShouldBe(1);
+            3.To(5).Count().ShouldBe(3);
+            3.To(1).Count().ShouldBe(3);
+            0.To(-10).Count().ShouldBe(11);
+        }
+    }
+}
---
+++ Sweetness/Ints.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sweetness
+{
+    public static class Ints
+    {
+        public static IEnumerable<int> To(this int from, int to)
+        {
+            if (to >= from)
+            {
+                for (var i = from; i <= to; i++)
+                {
+                    yield return i;
+                }
+            }
+            else
+            {
+                for (var i = from; i >= to; i--)
+                {
+                    yield return i;
+                }
+            }
+        }
+
+        public static IEnumerable<T> Times<T>(this int num, T toReturn)
+        {
+            for (var i = 0; i < num; i++)
+            {
+                yield return toReturn;
+            }
+        }
+
+        public static IEnumerable<T> Times<T>(this int num, Func<int, T> block)
+        {
+            for (var i = 0; i < num; i++)
+            {
+                yield return block(i);
+            }
+        }
+    }
+}
